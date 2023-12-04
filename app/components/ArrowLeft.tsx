8f2a62e0a5e5e8e97560a61881e5e65cd439473f
Lubos Kolumber <lubosk158@gmail.com>
2023-12-04 22:50:35 +0100

    Downgrade sanity

--- app/components/ArrowLeft.tsx
+++ app/components/ArrowLeft.tsx
@@ -10,8 +10,8 @@ const ArrowLeft = () => {
       xmlns="http://www.w3.org/2000/svg"
     >
       <path
-        fill-rule="evenodd"
-        clip-rule="evenodd"
+        fillRule="evenodd"
+        clipRule="evenodd"
         d="M8.27838 16.6101L8.6764 17L9.47243 16.2201L9.07442 15.8302L2.15496 9.05144L25 9.05144L25 7.94856L2.15496 7.94856L9.07442 1.16978L9.47244 0.779851L8.6764 -1.42705e-06L8.27838 0.389924L0.398021 8.11007L7.43094e-07 8.5L0.39802 8.88992L8.27838 16.6101Z"
         fill="black"
       />
